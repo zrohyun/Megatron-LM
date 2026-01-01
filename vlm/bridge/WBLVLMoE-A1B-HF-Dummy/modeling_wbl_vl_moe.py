9a75593855ba8cda0c3094bdc3d89424684d1db5
--global <jadepark13@gmail.com>
2026-01-01 22:28:49 +0900

    [fix] modeling.py

--- vlm/bridge/WBLVLMoE-A1B-HF-Dummy/modeling_wbl_vl_moe.py
+++ vlm/bridge/WBLVLMoE-A1B-HF-Dummy/modeling_wbl_vl_moe.py
@@ -1334,7 +1334,7 @@ class WBLVLMoEForCausalLM(WBLVLMoEPreTrainedModel, GenerationMixin):
         hidden_states = outputs.last_hidden_state
         # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
         slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
-        logits = self.lm_head(hidden_states[:, slice_indices, :]).to(self.lm_head.weight.dtype)
+        logits = self.lm_head(hidden_states[:, slice_indices, :].to(self.lm_head.weight.dtype))
 
         loss = None
         if labels is not None:
